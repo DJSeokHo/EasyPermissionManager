e5a6cf7a2edf02fc6f02e2923e319215f53d1e8c
CodingWithCat <djseokho@vip.qq.com>
2021-08-19 15:54:48 +0900

    update

--- app/src/main/java/com/swein/easypermissionmanager/EasyPermissionManager.kt
+++ app/src/main/java/com/swein/easypermissionmanager/EasyPermissionManager.kt
@@ -12,7 +12,6 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
-import java.lang.ref.WeakReference
 import java.util.*
 
 /**
@@ -21,7 +20,6 @@ import java.util.*
  */
 class EasyPermissionManager(private val componentActivity: ComponentActivity) {
 
-    private var activityWeakReference: WeakReference<Activity>? = null
     private var runnable: Runnable? = null
 
     private val permissionNotGrantedList = mutableListOf<String>()
@@ -35,94 +33,80 @@ class EasyPermissionManager(private val componentActivity: ComponentActivity) {
 
     init {
 
-        activityPermissionResult = registerActivityResult(componentActivity)
-        activityPermission = registerPermission(componentActivity)
+        activityPermissionResult = registerActivityResult()
+        activityPermission = registerPermission()
 
     }
 
-    private fun registerPermission(componentActivity: ComponentActivity): ActivityResultLauncher<Array<String>> {
-
+    private fun registerPermission(): ActivityResultLauncher<Array<String>> {
 
         return componentActivity.registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
 
-            activityWeakReference?.get()?.let { activity ->
+            val deniedPermissionList = mutableListOf<String>()
 
-                val deniedPermissionList = mutableListOf<String>()
+            it.entries.forEach { permissionResultMap ->
 
-                it.entries.forEach { permissionResultMap ->
+                val permission = permissionResultMap.key
+                val grantResult = permissionResultMap.value
 
-                    val permission = permissionResultMap.key
-                    val grantResult = permissionResultMap.value
+                if(!grantResult) {
 
-                    if(!grantResult) {
-
-                        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
-                            // if users denied permission, should request again
-                            deniedPermissionList.add(permission)
-                        }
-                        else {
-                            // if users denied permission twice, than go to app setting page
-                            AlertDialog.Builder(activity).apply {
-
-                                this.setTitle(title)
-                                this.setMessage(message)
-                                this.setPositiveButton(positiveButtonTitle) { _: DialogInterface?, _: Int ->
-
-                                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                                    val uri = Uri.fromParts("package", activity.packageName, null)
-                                    intent.data = uri
-                                    activityPermissionResult.launch(intent)
-                                }
-                                this.create()
-                                this.show()
-                            }
-
-                            return@let
-                        }
-
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(componentActivity, permission)) {
+                        // if users denied permission, should request again
+                        deniedPermissionList.add(permission)
                     }
+                    else {
+                        // if users denied permission twice, than go to app setting page
+                        AlertDialog.Builder(componentActivity).apply {
+
+                            this.setTitle(title)
+                            this.setMessage(message)
+                            this.setPositiveButton(positiveButtonTitle) { _: DialogInterface?, _: Int ->
+
+                                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                                val uri = Uri.fromParts("package", componentActivity.packageName, null)
+                                intent.data = uri
+                                activityPermissionResult.launch(intent)
+                            }
+                            this.create()
+                            this.show()
+                        }
+
+                        return@registerForActivityResult
+                    }
+
                 }
+            }
 
-                if (deniedPermissionList.isEmpty()) {
-                    runnable?.run()
-                }
-
-                activityWeakReference?.clear()
-
+            if (deniedPermissionList.isEmpty()) {
+                runnable?.run()
             }
         }
     }
 
-    private fun registerActivityResult(componentActivity: ComponentActivity): ActivityResultLauncher<Intent> {
+    private fun registerActivityResult(): ActivityResultLauncher<Intent> {
 
         return componentActivity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
 
-            activityWeakReference?.get()?.let {
+            val deniedPermissionList = mutableListOf<String>()
 
-                val deniedPermissionList = mutableListOf<String>()
+            if (result.resultCode == Activity.RESULT_CANCELED) {
 
-                if (result.resultCode == Activity.RESULT_CANCELED) {
+                for (i in permissionNotGrantedList.indices) {
 
-                    for (i in permissionNotGrantedList.indices) {
-
-                        val grantResult = ActivityCompat.checkSelfPermission(it, permissionNotGrantedList[i])
-                        if (grantResult != PackageManager.PERMISSION_GRANTED) {
-                            deniedPermissionList.add(permissionNotGrantedList[i])
-                        }
+                    val grantResult = ActivityCompat.checkSelfPermission(componentActivity, permissionNotGrantedList[i])
+                    if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                        deniedPermissionList.add(permissionNotGrantedList[i])
                     }
                 }
+            }
 
-                if (deniedPermissionList.isEmpty()) {
-                    runnable?.run()
-                    permissionNotGrantedList.clear()
-                }
-
-                activityWeakReference?.clear()
-
+            if (deniedPermissionList.isEmpty()) {
+                runnable?.run()
+                permissionNotGrantedList.clear()
             }
 
         }
-
     }
 
     fun requestPermission(
@@ -151,7 +135,6 @@ class EasyPermissionManager(private val componentActivity: ComponentActivity) {
 
             // save the context and runnable
             runnable = runnableAfterPermissionGranted
-            activityWeakReference = WeakReference(componentActivity)
 
             activityPermission.launch(permissions)
         }
